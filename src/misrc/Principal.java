336a39f53ca8e772f5bcac0e9b6e35a2a86cb341
DATASAGASTEGUI <datasagamadrid@gmail.com>
2025-04-04 16:48:51 +0200

    Foto 16:48 Principal

---
+++ src/misrc/Principal.java
@@ -0,0 +1,17 @@
+/*
+Construir un programa con un menú con dos opciones:
+1. Convertir Grados Fahrenheit a Celsius
+2. Convertir Celsius a Fahrenheit
+*/
+package misrc;
+
+
+public class Principal {
+
+    public static void main(String[] args) {
+      
+        
+        
+    }
+    
+}
