f0e0f40808440c805785b5018bd2d36381532bc2
DATASAGASTEGUI <datasagamadrid@gmail.com>
2025-04-04 17:48:13 +0200

    Foto 17:59 Proyecto Final

--- src/misrc/MisMetodos.java
+++ src/misrc/MisMetodos.java
@@ -1,13 +1,23 @@
 package misrc;
 
+import java.util.Scanner;
+
 
 public class MisMetodos {
     
     public static void convertirFahrenheitCelsius() {
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Ingresar Grados Fahrenheit");
+        double f = sc.nextDouble();
+        double c = (f-32)*5.0/9;
+        System.out.format("Celsius: %.2f\n", c);
     }
     
     public static void convertirCelsiusFahrenheit() {
-        
+       Scanner sc = new Scanner(System.in);
+       System.out.print("Ingresar Grados Celsius");
+       double c = sc.nextDouble();
+       double f = c * 9 / 5.0 + 32;
+       System.out.format("Fahrenheit: %.2f\n", f);
     }
 }
