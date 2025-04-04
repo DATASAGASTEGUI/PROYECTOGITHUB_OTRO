431ad781abac36051384a2a8247ac8d572e8c9c6
DATASAGASTEGUI <datasagamadrid@gmail.com>
2025-04-04 19:53:20 +0200

    Ultimos cambios

---
+++ src/misrc/Probando.java
@@ -0,0 +1,24 @@
+package misrc;
+
+import javax.swing.JOptionPane;
+
+public class Probando {
+
+public static void main(String[] args) {
+
+        int altura;
+        int ancho;
+        float resultArea;
+        float resultPerimetro;
+
+        altura = Integer.parseInt(JOptionPane.showInputDialog(null,"Ingrese la altura"));
+        ancho = Integer.parseInt(JOptionPane.showInputDialog(null,"Ingrese el ancho"));
+
+        resultArea = altura * ancho;
+        resultPerimetro = (altura+ancho) * 2;
+
+        JOptionPane.showMessageDialog(null, "Area: " +resultArea+ " Perimetro: " +resultPerimetro);
+
+    }
+    
+}
