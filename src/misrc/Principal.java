8c4ce11608caf9626a8f736e3a7f1badce541615
DATASAGASTEGUI <datasagamadrid@gmail.com>
2025-04-04 17:07:21 +0200

    Foto 17:06 Ingresar opción

--- src/misrc/Principal.java
+++ src/misrc/Principal.java
@@ -5,15 +5,23 @@ Construir un programa con un menú con dos opciones:
 */
 package misrc;
 
+import java.util.Scanner;
+
 
 public class Principal {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String opcion;
         while(true) {
            System.out.println("[1] Convertir Grados Fahrenheit a Celsius");
            System.out.println("[2] Convertir Celsius a Fahrenheit");
            System.out.println("[3] Salir");
+           
+           System.out.println("Ingresar opción? ");
+           opcion = sc.next();
         }
+        
     }
     
 }
